4d55350b6416842d44ed882b1a3cfcdec77fc159
Gaychey <tgaychey@gmail.com>
2019-07-19 01:54:00 -0400

    Updates

---
+++ precache-manifest.d57a38be339c2118a08e4a790c7dc53c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a57e592ef7c21378202082bc590de6",
+    "url": "/rest-api-practice/index.html"
+  },
+  {
+    "revision": "2ba88dd80ef5fa1d46cf",
+    "url": "/rest-api-practice/static/css/main.7f057ab4.chunk.css"
+  },
+  {
+    "revision": "d538652694bd439d49af",
+    "url": "/rest-api-practice/static/js/2.f2f882f0.chunk.js"
+  },
+  {
+    "revision": "2ba88dd80ef5fa1d46cf",
+    "url": "/rest-api-practice/static/js/main.e30ad128.chunk.js"
+  },
+  {
+    "revision": "2e37841ee267a1185533",
+    "url": "/rest-api-practice/static/js/runtime~main.200edffb.js"
+  }
+]);
